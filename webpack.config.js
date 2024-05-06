d8a9b956756d6be4e7330f2f7d1a97fbdd202dfc
Anastasia Kapustina <nikitinaan98@mail.ru>
2024-05-06 13:42:11 +0300

    feat: add git pages

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ const config = {
     // Learn more about plugins from https://webpack.js.org/configuration/plugins/
     new DefinePlugin({
       'process.env.DEVELOPMENT': !isProduction,
-      'process.env.API_ORIGIN': JSON.stringify(process.env.API_ORIGIN ?? '')
+      'process.env.API_ORIGIN': JSON.stringify(process.env.API_ORIGIN ?? 'https://larek-api.nomoreparties.co')
     })
   ],
   module: {
